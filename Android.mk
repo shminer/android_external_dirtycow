b59d331a2622eea4ec863788c6471d5e8aa1347e
James Christopher Adduono <jc@adduono.com>
2016-10-27 07:13:35 -0400

    Fix Android.mk

--- Android.mk
+++ Android.mk
@@ -1,25 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-  dirtycow.c
-
 LOCAL_MODULE := dirtycow
-LOCAL_LDFLAGS   += -llog
-LOCAL_CFLAGS    += -DDEBUG
-LOCAL_CFLAGS    += -fPIE
-LOCAL_LDFLAGS   += -fPIE -pie
-
+LOCAL_SRC_FILES := \
+	dirtycow.c
+LOCAL_CFLAGS += -DDEBUG
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SDK_VERSION := 21
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-
+LOCAL_MODULE := cow-run-as
+LOCAL_MODULE_FILE := run-as
 LOCAL_SRC_FILES := \
-  run-as.c
-
-LOCAL_MODULE := run-as
-LOCAL_CFLAGS    += -fPIE
-LOCAL_LDFLAGS   += -fPIE -pie
-
+	run-as.c
+LOCAL_SDK_VERSION := 21
 include $(BUILD_EXECUTABLE)
